0ac82d4d7bd4574bf58b14857c2ccd4b679ae4d8
moljac <mcvjetko@holisticware.net>
2016-02-21 23:12:35 +0100

    Sinch.Video.Calling/

---
+++ java2csharp/java/Sinch/Sinch.Video.Calling/PlaceCallActivity.java
@@ -0,0 +1,101 @@
+package com.sinch.android.rtc.sample.calling;
+
+import com.sinch.android.rtc.MissingPermissionException;
+import com.sinch.android.rtc.calling.Call;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class PlaceCallActivity extends BaseActivity {
+
+    private Button mCallButton;
+    private EditText mCallName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        mCallName = (EditText) findViewById(R.id.callName);
+        mCallButton = (Button) findViewById(R.id.callButton);
+        mCallButton.setEnabled(false);
+        mCallButton.setOnClickListener(buttonClickListener);
+
+        Button stopButton = (Button) findViewById(R.id.stopButton);
+        stopButton.setOnClickListener(buttonClickListener);
+    }
+
+    @Override
+    protected void onServiceConnected() {
+        TextView userName = (TextView) findViewById(R.id.loggedInName);
+        userName.setText(getSinchServiceInterface().getUserName());
+        mCallButton.setEnabled(true);
+    }
+
+    @Override
+    public void onDestroy() {
+        if (getSinchServiceInterface() != null) {
+            getSinchServiceInterface().stopClient();
+        }
+        super.onDestroy();
+    }
+
+    private void stopButtonClicked() {
+        if (getSinchServiceInterface() != null) {
+            getSinchServiceInterface().stopClient();
+        }
+        finish();
+    }
+
+    private void callButtonClicked() {
+        String userName = mCallName.getText().toString();
+        if (userName.isEmpty()) {
+            Toast.makeText(this, "Please enter a user to call", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        try {
+            Call call = getSinchServiceInterface().callUser(userName);
+            String callId = call.getCallId();
+            Intent callScreen = new Intent(this, CallScreenActivity.class);
+            callScreen.putExtra(SinchService.CALL_ID, callId);
+            startActivity(callScreen);
+        } catch (MissingPermissionException e) {
+            ActivityCompat.requestPermissions(this, new String[]{e.getRequiredPermission()}, 0);
+        }
+
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "You may now place a call", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "This application needs permission to use your microphone to function properly.", Toast
+                    .LENGTH_LONG).show();
+        }
+    }
+
+    private OnClickListener buttonClickListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.callButton:
+                    callButtonClicked();
+                    break;
+
+                case R.id.stopButton:
+                    stopButtonClicked();
+                    break;
+
+            }
+        }
+    };
+}
