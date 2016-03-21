e86cc2f638b2caec82999137fa075b8932073265
moljac <mcvjetko@holisticware.net>
2016-03-21 08:52:35 +0100

    InfinitePageViewer samples

---
+++ java2csharp/csharp/InfiniteViewPager/ColourFragment.cs
@@ -0,0 +1,48 @@
+﻿namespace com.antonyt.infiniteviewpager
+{
+
+	using Color = android.graphics.Color;
+	using Bundle = android.os.Bundle;
+	using Fragment = android.support.v4.app.Fragment;
+	using Gravity = android.view.Gravity;
+	using LayoutInflater = android.view.LayoutInflater;
+	using View = android.view.View;
+	using ViewGroup = android.view.ViewGroup;
+	using TextView = android.widget.TextView;
+
+	/// <summary>
+	/// Example Fragment class that shows an identifier inside a TextView.
+	/// </summary>
+	public class ColourFragment : Fragment
+	{
+
+		private int identifier;
+		private int colour;
+
+		public override void onCreate(Bundle savedInstanceState)
+		{
+			base.onCreate(savedInstanceState);
+			Bundle args = Arguments;
+			colour = args.getInt("colour");
+			identifier = args.getInt("identifier");
+		}
+
+		public override View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+		{
+			TextView v = new TextView(Activity);
+			v.Gravity = Gravity.CENTER;
+			v.TextSize = 40;
+			v.TextColor = Color.BLACK;
+			v.BackgroundColor = colour;
+			v.Text = "Fragment ID: " + identifier;
+			return v;
+		}
+
+		public override void onSaveInstanceState(Bundle outState)
+		{
+			base.onSaveInstanceState(outState);
+			outState.putBoolean("dummy", true);
+		}
+	}
+
+}
---
+++ java2csharp/csharp/InfiniteViewPager/InfiniteViewPagerActivity.cs
@@ -0,0 +1,62 @@
+﻿namespace com.antonyt.infiniteviewpager
+{
+
+	using Color = android.graphics.Color;
+	using Bundle = android.os.Bundle;
+	using Fragment = android.support.v4.app.Fragment;
+	using FragmentActivity = android.support.v4.app.FragmentActivity;
+	using FragmentPagerAdapter = android.support.v4.app.FragmentPagerAdapter;
+	using PagerAdapter = android.support.v4.view.PagerAdapter;
+	using ViewPager = android.support.v4.view.ViewPager;
+
+	public class InfiniteViewPagerActivity : FragmentActivity
+	{
+
+		public override void onCreate(Bundle savedInstanceState)
+		{
+			base.onCreate(savedInstanceState);
+			ContentView = R.layout.main;
+
+			PagerAdapter adapter = new FragmentPagerAdapterAnonymousInnerClassHelper(this, SupportFragmentManager);
+
+			// wrap pager to provide infinite paging with wrap-around
+			PagerAdapter wrappedAdapter = new InfinitePagerAdapter(adapter);
+
+			// actually an InfiniteViewPager
+			ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+			viewPager.Adapter = wrappedAdapter;
+
+		}
+
+		private class FragmentPagerAdapterAnonymousInnerClassHelper : FragmentPagerAdapter
+		{
+			private readonly InfiniteViewPagerActivity outerInstance;
+
+			public FragmentPagerAdapterAnonymousInnerClassHelper(InfiniteViewPagerActivity outerInstance, UnknownType getSupportFragmentManager) : base(getSupportFragmentManager)
+			{
+				this.outerInstance = outerInstance;
+				colours = new int[]{Color.CYAN, Color.GRAY, Color.MAGENTA, Color.LTGRAY, Color.GREEN, Color.WHITE, Color.YELLOW};
+			}
+
+			internal int[] colours;
+
+			public override int Count
+			{
+				get
+				{
+					return colours.length;
+				}
+			}
+
+			public override Fragment getItem(int position)
+			{
+				Fragment fragment = new ColourFragment();
+				Bundle args = new Bundle();
+				args.putInt("colour", colours[position]);
+				args.putInt("identifier", position);
+				fragment.Arguments = args;
+				return fragment;
+			}
+		}
+	}
+}
